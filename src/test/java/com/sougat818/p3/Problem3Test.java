7f53a4c9a841be8a0c139740c45a5b59c94c479e
Sougata Khan <sougat818@gmail.com>
2019-07-17 21:47:52 +1000

    Problem 3

---
+++ src/test/java/com/sougat818/p3/Problem3Test.java
@@ -0,0 +1,48 @@
+package com.sougat818.p3;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Problem3Test {
+
+  private Problem3 problem3;
+
+  @Before
+  public void setUp() {
+    problem3 = new Problem3();
+  }
+
+  @Test
+  public void testSolution1() {
+
+    ListNode listNode1 = new ListNode(new int[]{2, 4, 3});
+    ListNode listNode2 = new ListNode(new int[]{5, 6, 4});
+
+    Assert.assertEquals("708", problem3.addTwoNumbers(listNode1, listNode2).toString());
+  }
+
+  @Test
+  public void testSolution2() {
+
+    ListNode listNode1 = new ListNode(new int[]{});
+    ListNode listNode2 = new ListNode(new int[]{});
+
+    Assert.assertEquals("0", problem3.addTwoNumbers(listNode1, listNode2).toString());
+  }
+
+  @Test
+  public void testSolution3() {
+
+    ListNode listNode = new ListNode(
+        new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+            9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+            9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+            9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9});
+
+    Assert.assertEquals(
+        "8999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999991",
+        problem3.addTwoNumbers(listNode, listNode).toString());
+  }
+
+}
