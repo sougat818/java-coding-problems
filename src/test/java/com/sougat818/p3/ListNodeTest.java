bd7f5d840d12aef35e77e53d78dec13df1b7bacf
Sougata Khan <sougat818@gmail.com>
2019-07-18 07:43:32 +1000

    Add more unit tests

--- src/test/java/com/sougat818/p3/ListNodeTest.java
+++ src/test/java/com/sougat818/p3/ListNodeTest.java
@@ -1,9 +1,14 @@
 package com.sougat818.p3;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ListNodeTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   @Test
   public void toString1() {
@@ -27,5 +32,19 @@ public class ListNodeTest {
   public void toString4() {
     ListNode listNode = new ListNode(new int[]{1, 2, 3});
     Assert.assertEquals("123", listNode.toString());
+  }
+
+  @Test
+  public void toString5() {
+    expectedException.expectMessage("Value should be a single digit x - 10");
+    expectedException.expect(IllegalArgumentException.class);
+    new ListNode(new int[]{10});
+  }
+
+  @Test
+  public void toString6() {
+    expectedException.expectMessage("Value should be a single digit x - 10");
+    expectedException.expect(IllegalArgumentException.class);
+    new ListNode(10);
   }
 }
